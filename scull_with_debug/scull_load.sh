b6e3ed879bca07df752ff72518f56d634aba2cae
pengsida <291277604@qq.com>
2017-05-15 16:04:46 +0800

    update

--- scull_with_debug/scull_load.sh
+++ scull_with_debug/scull_load.sh
@@ -6,7 +6,7 @@
 #  Created by pengsida on 2017/5/8.
 #  Copyright © 2017年 pengsida. All rights reserved.
 
-·module="scull"
+module="scull"
 device="scull"
 nr_device=4
 
@@ -19,7 +19,7 @@ major=$(awk '/'${module}'/ {print $1}' /proc/devices)
 
 for((i = 0; i < ${nr_device}; i++))
 do
-mknod /dev/${device}$i c $major $i
+    mknod /dev/${device}$i c $major $i
 done
 
 group="staff"
